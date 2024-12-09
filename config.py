5f13dad2aafcac30e088f7f59cda7a089d7aeb04
moharanger <m33537924@gmail.com>
2024-12-09 07:22:13 +0330

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "7c331e3751b606fdffe1fad18f0065b6"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6323559178"))
-SUPPORT_GRP = "Poshtibaninetroplusbot"
-UPDATE_CHNL = "Fortis_dastorat_music"
-OWNER_USERNAME = "amirali_motlbi"
+OWNER_ID = int(getenv("OWNER_ID", "6663026922"))
+SUPPORT_GRP = "TORONTO_TM"
+UPDATE_CHNL = "Panel_Tornado"
+OWNER_USERNAME = "OWNER_ROYAL"
